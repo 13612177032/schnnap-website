a83c4ae1976b4ccebe8f1d28c3e0e8086e7d5848
qijian <763571230@qq.com>
2022-07-17 16:19:22 +0800

    auto commit

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
           @select="handleSelect"
           :router="router"
         >
-          <el-menu-item index="/">首页</el-menu-item>
+          <el-menu-item index="/">首页1</el-menu-item>
           <el-menu-item index="/news">新闻资讯</el-menu-item>
           <el-menu-item index="/product">产品中心</el-menu-item>
           <el-menu-item index="/case">经典案例</el-menu-item>
