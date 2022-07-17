d055ad1d85ff9d89ffb2aa438b42c910c6fd2d69
qijian <763571230@qq.com>
2022-07-17 16:32:51 +0800

    auto commit

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@
           @select="handleSelect"
           :router="router"
         >
-          <el-menu-item index="/">首页1</el-menu-item>
-          <el-menu-item index="/news">新闻资讯1</el-menu-item>
-          <el-menu-item index="/product">产品中心1</el-menu-item>
-          <el-menu-item index="/case">经典案例1</el-menu-item>
+          <el-menu-item index="/">首页</el-menu-item>
+          <el-menu-item index="/news">新闻资讯</el-menu-item>
+          <el-menu-item index="/product">产品中心</el-menu-item>
+          <el-menu-item index="/case">经典案例</el-menu-item>
           <el-menu-item index="/goin">走进科建</el-menu-item>
-          <el-menu-item index="/download">下载APP</el-menu-item>
+          <el-menu-item index="/download">下载APPxxxx</el-menu-item>
         </el-menu>
       </el-header>
       <el-main>
